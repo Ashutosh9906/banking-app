f3eeb0ab7111a93b560a84527a6a9f821374e329
Ashutosh9906 <ashutoshgandule1@gmail.com>
2025-07-20 17:43:16 +0530

    Applied some otp validation

---
+++ middlewares/validateEmail.js
@@ -0,0 +1,37 @@
+const Otp = require("../models/otp");
+const User = require("../models/user");
+
+async function validateEmail(req, res, next) {
+    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!pattern.test(req.body.email)) {
+        return res.status(400).json({ msg: "Invalid Email" })
+    }
+    next();
+}
+
+async function otpCoolDown(req, res, next) {
+    const oneMinute = 60 * 1000;
+    const email = req.body.email;
+    const user = await Otp.findOne({ email });
+    // console.log(user.updatedAt.getTime());
+    if (user && (Date.now() < (user.updatedAt.getTime() + oneMinute))) {
+        return res.status(400).json({ msg: "CoolDown Period" })
+    }
+    next();
+}
+
+async function validateOtp(req, res, next){
+    const fiveMinutes = 5 * 60 * 1000;
+    const email = req.body.email;
+    const user = await Otp.findOne({ email });
+    if(!(Date.now() < (user.updatedAt.getTime() + fiveMinutes))){
+        return res.status(400).json({ msg: "Otp Expired, Try Again" })
+    }
+    next();
+}
+
+module.exports = {
+    validateEmail,
+    otpCoolDown,
+    validateOtp
+};
